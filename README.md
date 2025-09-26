d83c3f5c23e7a13f9a6ec5ef4588e71cf1620550
ZeGollyGosh <cascolucci@gmail.com>
2025-09-26 14:58:57 -0400

    Made some adjustments to try something new and updated the readme with some notes

--- README.md
+++ README.md
@@ -26,9 +26,12 @@ Instead of doing a hard reset, there's now more of a decay so there's not so muc
 
 ### Optimistic Plans
 
-- **Controller Mocking**: As fer sler mentioned, using an openxr library to control the game may be better, or mocking more of the controller. Depending on the bindings needed, one could, theoretically, make it so sprinting actually triggers the sprint action of the game you're playing once you get to a certain speed. The current problem, though, is the jankiness of the setup. We're losing track of how fast the user is going after a certain speed, so we may have to settle for a "oh I've lost track of how fast you're going, I guess you're sprinting now" mode. Still in the process of fine tuning things.
+- **Controller Mocking**: As fer sler mentioned, using an openxr library to control the game may be better, or mocking more of the controller. Depending on the bindings needed, one could, theoretically, make it so sprinting actually triggers the sprint action of the game you're playing once you get to a certain speed. The current problem, though, is the jankiness of the setup. We're losing track of how fast the user is going after a certain speed, so we may have to settle for a "oh I've lost track of how fast you're going, I guess you're sprinting now" mode. Still in the process of fine tuning things
 
 ## Golly's Notes
 
+- On 9/26 at around 2:50PM (during another lunch :P): Realized a major flaw during lunch when trying to play Valheim. We're mocking two controllers into one, and I don't think this works on all games. There are a few possibilities on how to work this, but none of them are very ideal. Here's what I'm thinking:
+  - Get HIDHide and hide the controller inputs from all games, then have this program create a virtual oculus controller set and map through all the controller input that it sees from the real oculus controllers, EXCEPT the left analog stick, which it will use some algorithm to read the mouse movements for (still a work in progress). Would need to be configured for any other controllers, and is going to be obnoxious to sort through.
+  - Use Universal Control Remapper or some similar tool to remap both the xbox 360 controller input, and the oculus input, to one output. Seems to be several years out of date but I'm a C# dev so I may be able to figure it out?
 - On 9/25 sometime in the evening I tested it and it worked much more gradually in speed but I'm not totally happy with it, trying new things tomorrow
 - As of 9/25 1:15PM EST I haven't had a chance to test this out properly. It theoretically works but I need to hop in-game to test it and I'm technically on lunch rn :P
--- treadmill.py
+++ treadmill.py
@@ -2,25 +2,27 @@ import time
 from pynput.mouse import Button, Controller
 import vgamepad as vg
 from collections import deque
-
+'''
+    Quick note: to end this program hit ctrl+c or ctrl+alt+delete on windows
+    if you need to end it abruptly. It should stop the controller and the program
+'''
 gamepad = vg.VX360Gamepad()
 mouse = Controller()
-
 # To track change in position (delta)
 last_mouse_y = 500
 accumulated_value = 0
-
 # Speed tracking
 mouse_positions = deque(maxlen=10)  # Store last 10 position samples
 timestamps = deque(maxlen=10) # Store the corresponding timestamps
 peak_speed = 0
-
 # Parameters
 sensitivity = 7 # Try to stay between 1 and 50
-deadzone = 2 
+deadzone = 2
 decay_rate = 0.92 # How quickly the joystick returns to center
 max_speed = 32767 # Maximum joystick value in either positive or negative directions
-
+# NEW: Speed-based boost parameters
+min_output_threshold = 8000  # Minimum joystick value when moving (ensures slow walk registers)
+speed_deadzone = 10  # Minimum speed (px/s) to count as intentional movement
 # Visual Display Parameters
 max_pixel_speed = 1000 # Max speed for the bar display, pixels per second
 bar_width = 40 # Width of the speed bar
@@ -33,28 +35,24 @@ for i in range(10):
 def create_bar(value, max_value, width, char='█', empty_char='░'):
     if max_value == 0:
         return empty_char * width
-    
+   
     filled = int((value / max_value) * width)
     filled = min(filled, width)  # Cap at width
     empty = width - filled
-    
+   
     return char * filled + empty_char * empty
 
 while True:
     time.sleep(0.03)
-
     # Get the current position
     current_mouse_y = mouse.position[1]
     # Trying to calculate time for speed, maybe this will work
     current_time = time.time()
-
     # Get the change in position (delta)
     mouse_delta = (500 - current_mouse_y)
-
     # Add to speed tracking buffers
     mouse_positions.append(current_mouse_y)
     timestamps.append(current_time)
-
     # Calculate speed
     speed = 0
     if len(mouse_positions) >= 2:
@@ -62,49 +60,41 @@ while True:
         total_distance = 0
         for i in range(1, len(mouse_positions)):
             total_distance += abs(mouse_positions[i] - mouse_positions[i-1])
-
         # Calculate time span
         time_span = timestamps[-1] - timestamps[0]
-
         # Calculate pixels per second
         if time_span > 0:
             # I never thought I'd use the equation "speed is distance over time"
-            # But here it is 
-            speed = total_distance / time_span\
-
-    """
-        Thinking of how to do this next step
-        I want to have the speed of the treadmill more directly effect the joystick
-        I'm calculating the speed and I think I want to run on the treadmill and get
-        an idea of what speeds we're looking at for walking, jogging, and sprinting.
-
-        From there, I think I can get an idea of how to translate that into a good
-        joystick value so it doesn't go right to full throttle every time. This way we
-        can get speed variance between walking and running a bit more.
-
-        But I have to go to work now so I can't tinker with this anymore, dang it :(
-    """
-
+            # But here it is
+            speed = total_distance / time_span
     # Track peak speed
     if speed > peak_speed:
         peak_speed = speed
-
-    if abs(mouse_delta) < deadzone: 
+    
+    if abs(mouse_delta) < deadzone:
         mouse_delta = 0
-
+    
     accumulated_value += mouse_delta * sensitivity
     accumulated_value *= decay_rate
-    
+   
     mousey = max(-max_speed, min(max_speed, int(accumulated_value))) # convert mouse position to joystick value
     
+    # If we detect movement speed, ensure minimum output
+    if speed > speed_deadzone:
+        if mousey > 0 and mousey < min_output_threshold:
+            mousey = min_output_threshold
+        elif mousey < 0 and mousey > -min_output_threshold:
+            mousey = -min_output_threshold
+    else:
+        # If no real speed detected, decay faster to zero
+        accumulated_value *= 0.7
+   
     # Reset mouse to center
-    mouse.position = (700, 500) # COMMENT OUT THIS LINE WITH A # SYMBOL AT THE START OF THE LINE TO MAKE SETTING UP CONTROLLER BINDING EASIER
-    
-
+    mouse.position = (700, 500) # COMMENT THIS OUT WITH A # SYMBOL AT THE START OF THE LINE TO MAKE SETTING UP CONTROLLER BINDING EASIER
+   
     joystick_bar = create_bar(abs(mousey), max_speed, bar_width)
-
-    print(f"\rJOY: [{joystick_bar}]", end='', flush=True)
-    
+    print(f"\rSPD:{speed:4.0f} JOY: [{joystick_bar}] {mousey:5}", end='', flush=True)
+   
     gamepad.left_joystick(x_value=0, y_value=mousey)  # values between -32768 and 32767
-    
+   
     gamepad.update()
