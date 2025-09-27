02e2a33f6944870462b4e0c1e52eaaf580bfefe5
ZeGollyGosh <cascolucci@gmail.com>
2025-09-27 11:50:18 -0400

    Adding more notes and an empty project to toy with

---
+++ controllervirtualizer.py
@@ -0,0 +1,11 @@
+import pyopenxr as xr
+
+'''
+    GOAL: Transfer actual VR Controller inputs to Virtual controller inputs
+
+    RESEARCH:
+        Controller SDKs, OpenXR runtime, 
+
+    STEPS:
+        
+'''
